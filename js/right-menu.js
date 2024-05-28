144b3a370fdeb75117fa6ccfb7d0497d1b273232
이유찬 <delphinium04@outlook.com>
2024-05-28 14:01:14 +0900

    eat-yellow finish
    nav + menu + yellow 완성
    0528 2:01 맥북

--- js/right-menu.js
+++ js/right-menu.js
@@ -1,59 +1,31 @@
-/*
-$(document).ready(function () {
-    $('#menu-btn>a').on('click', function () {
-        console.log('menu_btn clicked')
-        $('.menu_bg').show();
-        $('.menu_menu').show().animate({
-            right: 0
-        });
-    });
-    $('.menu_close').on('click', function () {
-        $('.menu_bg').hide();
-        $('.menu_menu').animate({
-            right: '-' + 50 + '%'
-        }, function () {
-            $('.menu_menu').hide();
-        });
-    });
-});
+
+$menu_open_btn = "#menu-open-btn";
+$menu_container = "#menu-container";
+$menu_bg = "#menu-bg";
 
 $(document).ready(function () {
-    $('.menu_eat').click(function () {
-        $('.menu_eat_menu').toggle();
-    });
-});
-$(document).ready(function () {
-    $('.menu_tourism').click(function () {
-        $('.menu_tourism_menu').toggle();
-    });
-});
-$(document).ready(function () {
-    $('.menu_festival').click(function () {
-        $('.menu_festival_menu').toggle();
-    });
-});
-$(document).ready(function () {
-    $('.menu_activity').click(function () {
-        $('.menu_activity_menu').toggle();
-    });
-});
-*/
-
-$(document).ready(function () {
-    $('#menu-open-btn').on('click', function () {
-        $('#menu-container').addClass('active');
+    $($menu_open_btn).on('click', function () {
+        $($menu_container).addClass('active');
+        $($menu_container).removeClass('deactive');
+        $($menu_bg).addClass('active');
+        $($menu_bg).removeClass('deactive')
     })
 
     $('#menu-close-btn').on('click', function () {
-        $('#menu-container').removeClass('active');
+        $($menu_container).addClass('deactive');
+        $($menu_container).removeClass('active');
+        $($menu_bg).addClass('deactive');
+        $($menu_bg).removeClass('active')
     })
 
     $(".menu-submenu").click(function () {
+        // 이미 활성화되어있는 경우
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
             $(this).parent().find('.menu-submenu-content').removeClass('active');
         }
         else {
+            $(this).removeClass('deactive')
             $(this).addClass('active');
             $(this).parent().find('.menu-submenu-content').addClass('active');
         }
