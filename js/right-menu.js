6385f97941655612e2625ca16b4f813ed408fa57
이유찬 <delphinium04@outlook.com>
2024-05-31 18:50:21 +0900

    1차 문제 해결

--- js/right-menu.js
+++ js/right-menu.js
@@ -1,36 +1,39 @@
-
 $menu_open_btn = "#menu-open-btn";
 $menu_close_btn = "#menu-close-btn";
 $menu_container = "#menu-container";
 $menu_bg = "#menu-bg";
 
 $(document).ready(function () {
-    $($menu_open_btn).on('click', function () {
-        $($menu_container).addClass('active');
-        $($menu_container).removeClass('deactive');
-        $($menu_bg).addClass('active');
-        $($menu_bg).removeClass('deactive');
-        console.log('menu opened');
-    })
+    $('#nav').load('nav.html');
+    $('#menu').load('menu.html', function () {
+        $($menu_open_btn).on('click', function () {
+            $($menu_container).addClass('active');
+            $($menu_container).removeClass('deactive');
+            $($menu_bg).addClass('active');
+            $($menu_bg).removeClass('deactive');
+            console.log('menu opened');
+        })
 
-    $($menu_close_btn).on('click', function () {
-        $($menu_container).addClass('deactive');
-        $($menu_container).removeClass('active');
-        $($menu_bg).addClass('deactive');
-        $($menu_bg).removeClass('active');
-        console.log('menu closed');
-    })
+        $($menu_close_btn).on('click', function () {
+            $($menu_container).addClass('deactive');
+            $($menu_container).removeClass('active');
+            $($menu_bg).addClass('deactive');
+            $($menu_bg).removeClass('active');
+            console.log('menu closed');
+        })
 
-    $(".menu-submenu").click(function () {
-        // 이미 활성화되어있는 경우
-        if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
-            $(this).parent().find('.menu-submenu-content').removeClass('active');
-        }
-        else {
-            $(this).removeClass('deactive')
-            $(this).addClass('active');
-            $(this).parent().find('.menu-submenu-content').addClass('active');
-        }
+        $(".menu-submenu").click(function () {
+            console.log('menu clicked')
+            // 이미 활성화되어있는 경우
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+                $(this).parent().find('.menu-submenu-content').removeClass('active');
+            }
+            else {
+                $(this).removeClass('deactive')
+                $(this).addClass('active');
+                $(this).parent().find('.menu-submenu-content').addClass('active');
+            }
+        });
     });
-});
+})
